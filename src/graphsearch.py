130ac2fe86372fcbbadc89aa6f525f6592aaeffd
Dave Snowdon <dave@davesnowdon.com>
2020-01-04 13:02:21 +0000

    use argparse to parse command line and add option to write path to file

--- src/graphsearch.py
+++ src/graphsearch.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 import argparse
-import csv
 import sys
 
 from astar import AStarSearch, Graph, build_graph
 
-def main(args):
-    if len(args) != 4:
-        print("Usage: astar.py nodes.csv edges.csv start-node-id goal-node-id")
-        sys.exit(1)
-    graph = build_graph(args[0], args[1])
+def main():
+    parser = argparse.ArgumentParser(description="Use A* search to find the optimal path between two nodes on the graph")
+    parser.add_argument("nodes_filename", help="File containing node data in CSV format")
+    parser.add_argument("edges_filename", help="File containing edge data in CSV format")
+    parser.add_argument("start_node_id", help="ID of node to start search")
+    parser.add_argument("goal_node_id", help="ID of node to find path to")
+    parser.add_argument("-o", "--output", help="Write path to this file as comma separated list of node IDs")
+    args = parser.parse_args()
+
+    graph = build_graph(args.nodes_filename, args.edges_filename)
     print(f"Graph {graph.node_count()} nodes,  {graph.edge_count()} edges")
     search = AStarSearch(graph)
-    path, cost = search.find_path(args[2], args[3])
+    path, cost = search.find_path(args.start_node_id, args.goal_node_id)
     print(f"Path = {path}")
     print(f"Cost = {cost}")
 
+    if args.output:
+        with open(args.output, "w") as outfile:
+            node_ids = [node.id for node in path]
+            data = ",".join(node_ids)
+            outfile.write(data)
+
 if __name__== "__main__":
-    # ignore first argument which is just the python file
-    main(sys.argv[1:])
+    main()
